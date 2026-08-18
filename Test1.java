8a91cd40e0051e70d62f6ef030e9b4888e090453
Varshapriya HV <varshayashu1234@gmail.com>
2026-08-18 12:23:22 +0530

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,20 @@
+package day002;
+
+public class Test1 {
+
+ 		int a = 20;
+		int b = 10;
+
+		void m1(int c,int d) {
+			System.out.println("dfasdf "+(a+b));
+		System.out.println("asdfas"+(c+d));
+		}
+
+		public static void main(String[] args) {
+			Test   vv = new Test();
+			vv.m1(4,3);
+		}
+	}
+	
+
+
