ee792f912ae09ab0cd02dfc0dd58d95ca04c7fd9
Varshapriya HV <varshayashu1234@gmail.com>
2026-08-18 10:53:53 +0530

    Add files via upload

---
+++ Dmeo.java
@@ -0,0 +1,25 @@
+package day2;
+
+public class Dmeo {
+
+ 		// TODO Auto-generated method stub
+		// poly morphism
+		//method overloading
+		// method overriding
+
+  
+			void m1() {
+				System.out.println("sdfa");
+			}
+			void m1(int a) {
+				System.out.println("dfasdfasdfasfas");
+			}
+			public static void main(String[] args) {
+				Dmeo test = new Dmeo();// object
+				test.m1();
+				test.m1(3);
+			}
+		
+	}
+
+
